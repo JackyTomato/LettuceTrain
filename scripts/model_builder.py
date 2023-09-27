dc6d56c140772b0a543fca78712d3f6a77a04a21
Jacky To <jacky.to@wur.nl>
2023-09-27 11:04:57 +0200

    Added docstring

--- scripts/model_builder.py
+++ scripts/model_builder.py
@@ -17,21 +17,31 @@ import torchvision
 
 # Define nn.Module class for model
 class TipburnClassifier(nn.Module):
-    def __init__(self, n_classes, bb_name=None, pretrained_bb=True, freeze_bb=True):
+    def __init__(self, n_classes, bb_name=None, weights_bb=True, freeze_bb=True):
+        """Creates tipburn classifier as PyTorch nn.Module class
+
+        Creates a CNN with a backbone. Backbone can be pretrained and frozen.
+        Only supports the following backbones:
+
+        Args:
+            n_classes (int): Numbers of classes to predict.
+            bb_name (str, optional): _description_. Defaults to None.
+            weights_bb (bool, optional): _description_. Defaults to True.
+            freeze_bb (bool, optional): _description_. Defaults to True.
+
+        Raises:
+            Exception: _description_
+        """
         super(TipburnClassifier, self).__init__()
-
-        # We add the background class
-        self.n_classes = n_classes + 1
-
         # Backbone
         if bb_name is not None:
-            backbone_call = f"torchvision.models.{bb_name}(pretrained={pretrained_bb})"
+            backbone_call = f"torchvision.models.{bb_name}(weights={weights_bb})"
             backbone = eval(backbone_call)
             for param in backbone.parameters():
                 param.requires_grad = not freeze_bb
             # TODO: adjust final layers of backbone to suit model, prolly only support few models
         else:
-            raise Exception("No argument for bb_name, a backbone is required")
+            raise Exception("No argument for bb_name, a backbone is mandatory")
 
         # Layer 1
 
