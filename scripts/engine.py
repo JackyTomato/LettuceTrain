5f0bb1de77a0c0dc2cfa54bc2e9e585c42171794
Jacky To <jacky.to@wur.nl>
2023-09-28 17:08:21 +0200

    Copied, combined and adapted scripts

---
+++ scripts/engine.py
@@ -0,0 +1,205 @@
+"""
+Functionality for training and testing
+
+TODO:
+    - Implement checkpointing
+    - Maybe add training plot
+"""
+
+# Import statements
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+
+
+# Define train step function per epoch
+def train_step(model, dataloader, loss_fn, optimizer, scaler, device):
+    """Trains a PyTorch model for a single epoch.
+
+    Turns a PyTorch model to training mode and then runs through all of the required training steps:
+        1. forward pass, 2. loss calculation, 3. backward pass.
+
+    Uses automatic automatic mixed precision (AMP) training to utilize float16
+    in order to speed up training while minimizing precision loss.
+    AMP is performed using torch.cuda.amp.autocast() and a scaler.
+
+    Args:
+      model (nn.Module): A PyTorch model to be trained.
+      dataloader (torch.utils.data.DataLoader): A DataLoader instance for the model to be trained on.
+      loss_fn (nn.Module): A PyTorch loss function to minimize.
+      optimizer (torch.optim.Optimizer): A PyTorch optimizer to help minimize the loss function.
+      scaler (torch.cuda.amp.GradScaler): A PyTorch gradient scaler to help minimize gradient underflow.
+      device (torch.device): A target device to compute on (e.g. "cuda" or "cpu").
+
+    Returns:
+      A tuple of training loss and training accuracy metrics.
+      In the form (train_loss, train_accuracy). For example:
+
+      (0.1112, 0.8743)
+    """
+    # Put model in train mode
+    model.train()
+
+    # Setup train loss and train accuracy values
+    train_loss, train_acc = 0, 0
+
+    # Setup tdqm loop for progress bar
+    loop = tqdm(dataloader)
+
+    # Loop through data loader data batches
+    for batch, (data, labels) in enumerate(loop):
+        # Send data to target device
+        data, labels = data.to(device), labels.to(device)
+
+        # 1. Forward pass
+        with torch.cuda.amp.autocast():
+            preds = model(data)
+
+            # 2. Calculate  and accumulate loss
+            loss = loss_fn(preds, labels)
+            train_loss += loss.item()
+
+        # 3. Backward pass
+        optimizer.zero_grad()  # Sets gradient to zero
+        scaler.scale(loss).backward()  # Calculate gradient
+        scaler.step(optimizer)  # Updates weights using gradient
+        scaler.update()
+
+        # Calculate and accumulate accuracy metric across all batches
+        pred_class = torch.argmax(torch.softmax(preds, dim=1), dim=1)
+        train_acc += (pred_class == labels).sum().item() / len(preds)
+
+        # Update tqdm loop
+        loop.set_postfix(loss=loss.item())
+
+    # Adjust metrics to get average loss and accuracy per batch
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
+
+
+# Define test step function per epoch
+def test_step(model, dataloader, loss_fn, device):
+    """Tests a PyTorch model for a single epoch.
+
+    Turns a PyTorch model to eval mode and then performs a forward pass on a testing dataset.
+    The test step does not apply a sigmoid function, thus a loss function appropiate for logits
+    must be used.
+
+    Args:
+      model (nn.Module): A PyTorch model to be tested.
+      dataloader (torch.utils.data.DataLoader): A DataLoader instance for the model to be tested on.
+      loss_fn (nn.Module): A PyTorch loss function to calculate loss on the test data.
+      device (torch.device): A target device to compute on (e.g. "cuda" or "cpu").
+
+    Returns:
+      A tuple of testing loss and testing accuracy metrics.
+      In the form (test_loss, test_accuracy). For example:
+
+      (0.0223, 0.8985)
+    """
+    # Put model in eval mode
+    model.eval()
+
+    # Setup test loss and test accuracy values
+    test_loss, test_acc = 0, 0
+
+    # Turn on inference context manager
+    with torch.inference_mode():
+        # Loop through DataLoader batches
+        for batch, (data, labels) in enumerate(dataloader):
+            # Send data to target device
+            data, labels = data.to(device), labels.to(device)
+
+            # 1. Forward pass
+            test_pred_logits = model(data)
+
+            # 2. Calculate and accumulate loss (logits haven't been through sigmoid!)
+            loss = loss_fn(test_pred_logits, labels)
+            test_loss += loss.item()
+
+            # Calculate and accumulate accuracy
+            test_pred_labels = test_pred_logits.argmax(dim=1)
+            test_acc += (test_pred_labels == labels).sum().item() / len(
+                test_pred_labels
+            )
+
+    # Adjust metrics to get average loss and accuracy per batch
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
+
+
+# Define train function to loop over epochs
+def train(
+    model, train_dataloader, test_dataloader, optimizer, scaler, loss_fn, epochs, device
+):
+    """Trains and tests a PyTorch model for a given number of epochs.
+
+    Passes a PyTorch model through train_step() and test_step()
+    functions for a number of epochs, training and testing the model
+    in each iteration of the epoch loop.
+
+    Calculates, prints and stores evaluation metrics throughout.
+
+    Args:
+      model (nn.Module): A PyTorch model to be trained and tested.
+      train_dataloader (torch.utils.data.DataLoader): A DataLoader instance for the model to be trained on.
+      test_dataloader (torch.utils.data.DataLoader): A DataLoader instance for the model to be tested on.
+      optimizer (torch.optim.Optimizer): A PyTorch optimizer to help minimize the loss function.
+      scaler (torch.cuda.amp.GradScaler): A PyTorch gradient scaler to help minimize gradient underflow.
+      loss_fn (nn.Module): A PyTorch loss function to calculate loss on both datasets.
+      epochs (int): An integer indicating how many epochs to train for.
+      device (torch.device): A target device to compute on (e.g. "cuda" or "cpu").
+
+    Returns:
+      A dictionary of training and testing loss as well as training and
+      testing accuracy metrics. Each metric has a value in a list for
+      each epoch.
+      In the form: {train_loss: [...],
+                    train_acc: [...],
+                    test_loss: [...],
+                    test_acc: [...]}
+      For example if training for epochs=2:
+                   {train_loss: [2.0616, 1.0537],
+                    train_acc: [0.3945, 0.3945],
+                    test_loss: [1.2641, 1.5706],
+                    test_acc: [0.3400, 0.2973]}
+    """
+    # Create empty results dictionary
+    results = {"train_loss": [], "train_acc": [], "test_loss": [], "test_acc": []}
+
+    # Loop through training and testing steps for a number of epochs with tqdm progress bar
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(
+            model=model,
+            dataloader=train_dataloader,
+            loss_fn=loss_fn,
+            optimizer=optimizer,
+            scaler=scaler,
+            device=device,
+        )
+        test_loss, test_acc = test_step(
+            model=model,
+            dataloader=test_dataloader,
+            loss_fn=loss_fn,
+            device=device,
+        )
+
+        # Print out what's happening
+        print(
+            f"Epoch: {epoch+1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+        # Update results dictionary
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+
+    # Return the filled results at the end of the epochs
+    return results
