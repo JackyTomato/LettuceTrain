11b76678c0b31f70dd864f7e293051c473dd920c
Jacky To <jacky.to@wur.nl>
2023-09-29 15:29:04 +0200

    Customizability performance function, acc in utils

--- scripts/engine.py
+++ scripts/engine.py
@@ -13,11 +13,14 @@ from tqdm import tqdm
 
 
 # Define train step function per epoch
-def train_step(model, dataloader, loss_fn, optimizer, scaler, device):
+def train_step(model, dataloader, loss_fn, performance_fn, optimizer, scaler, device):
     """Trains a PyTorch model for a single epoch.
 
     Turns a PyTorch model to training mode and then runs through all of the required training steps:
         1. forward pass, 2. loss calculation, 3. backward pass.
+
+    The test step does not apply a sigmoid/softmax function, thus a loss function appropiate
+    for the logits must be used.
 
     Uses automatic automatic mixed precision (AMP) training to utilize float16
     in order to speed up training while minimizing precision loss.
@@ -27,21 +30,22 @@ def train_step(model, dataloader, loss_fn, optimizer, scaler, device):
       model (nn.Module): A PyTorch model to be trained.
       dataloader (torch.utils.data.DataLoader): A DataLoader instance for the model to be trained on.
       loss_fn (nn.Module): A PyTorch loss function to minimize.
+      performance_fn (function): A function that calculates a performance metric, e.g. class accuracy in utils.
       optimizer (torch.optim.Optimizer): A PyTorch optimizer to help minimize the loss function.
       scaler (torch.cuda.amp.GradScaler): A PyTorch gradient scaler to help minimize gradient underflow.
       device (torch.device): A target device to compute on (e.g. "cuda" or "cpu").
 
     Returns:
-      A tuple of training loss and training accuracy metrics.
-      In the form (train_loss, train_accuracy). For example:
+      A tuple of training loss and training performance metrics.
+      In the form (train_loss, train_performance). For example:
 
       (0.1112, 0.8743)
     """
     # Put model in train mode
     model.train()
 
-    # Setup train loss and train accuracy values
-    train_loss, train_acc = 0, 0
+    # Setup train loss and train performance values
+    train_loss, train_perform = 0, 0
 
     # Setup tdqm loop for progress bar
     loop = tqdm(dataloader)
@@ -53,10 +57,10 @@ def train_step(model, dataloader, loss_fn, optimizer, scaler, device):
 
         # 1. Forward pass
         with torch.cuda.amp.autocast():
-            preds = model(data)
+            pred_logits = model(data)
 
             # 2. Calculate  and accumulate loss
-            loss = loss_fn(preds, labels)
+            loss = loss_fn(pred_logits, labels)
             train_loss += loss.item()
 
         # 3. Backward pass
@@ -65,35 +69,35 @@ def train_step(model, dataloader, loss_fn, optimizer, scaler, device):
         scaler.step(optimizer)  # Updates weights using gradient
         scaler.update()
 
-        # Calculate and accumulate accuracy metric across all batches
-        pred_class = torch.argmax(torch.softmax(preds, dim=1), dim=1)
-        train_acc += (pred_class == labels).sum().item() / len(preds)
+        # Calculate and accumulate performance metric across all batches
+        train_perform += performance_fn(pred_logits, labels)
 
         # Update tqdm loop
         loop.set_postfix(loss=loss.item())
 
-    # Adjust metrics to get average loss and accuracy per batch
+    # Adjust metrics to get average loss and performance per batch
     train_loss = train_loss / len(dataloader)
-    train_acc = train_acc / len(dataloader)
-    return train_loss, train_acc
+    train_perform = train_perform / len(dataloader)
+    return train_loss, train_perform
 
 
 # Define test step function per epoch
-def test_step(model, dataloader, loss_fn, device):
+def test_step(model, dataloader, loss_fn, performance_fn, device):
     """Tests a PyTorch model for a single epoch.
 
     Turns a PyTorch model to eval mode and then performs a forward pass on a testing dataset.
-    The test step does not apply a sigmoid function, thus a loss function appropiate for logits
-    must be used.
+    The test step does not apply a sigmoid/softmax function, thus a loss function appropiate
+    for the logits must be used.
 
     Args:
       model (nn.Module): A PyTorch model to be tested.
       dataloader (torch.utils.data.DataLoader): A DataLoader instance for the model to be tested on.
       loss_fn (nn.Module): A PyTorch loss function to calculate loss on the test data.
+      performance_fn (function): A function that calculates a performance metric, e.g. class accuracy in utils.
       device (torch.device): A target device to compute on (e.g. "cuda" or "cpu").
 
     Returns:
-      A tuple of testing loss and testing accuracy metrics.
+      A tuple of testing loss and testing performance metrics.
       In the form (test_loss, test_accuracy). For example:
 
       (0.0223, 0.8985)
@@ -101,8 +105,8 @@ def test_step(model, dataloader, loss_fn, device):
     # Put model in eval mode
     model.eval()
 
-    # Setup test loss and test accuracy values
-    test_loss, test_acc = 0, 0
+    # Setup test loss and test performance values
+    test_loss, test_perform = 0, 0
 
     # Turn on inference context manager
     with torch.inference_mode():
@@ -114,25 +118,29 @@ def test_step(model, dataloader, loss_fn, device):
             # 1. Forward pass
             test_pred_logits = model(data)
 
-            # 2. Calculate and accumulate loss (logits haven't been through sigmoid!)
+            # 2. Calculate and accumulate loss
             loss = loss_fn(test_pred_logits, labels)
             test_loss += loss.item()
 
-            # Calculate and accumulate accuracy
-            test_pred_labels = test_pred_logits.argmax(dim=1)
-            test_acc += (test_pred_labels == labels).sum().item() / len(
-                test_pred_labels
-            )
+            # Calculate and accumulate performance
+            test_perform += performance_fn(test_pred_logits, labels)
 
-    # Adjust metrics to get average loss and accuracy per batch
+    # Adjust metrics to get average loss and performance per batch
     test_loss = test_loss / len(dataloader)
-    test_acc = test_acc / len(dataloader)
-    return test_loss, test_acc
+    test_perform = test_perform / len(dataloader)
+    return test_loss, test_perform
 
 
 # Define train function to loop over epochs
 def train(
-    model, train_dataloader, test_dataloader, optimizer, scaler, loss_fn, epochs, device
+    model,
+    train_dataloader,
+    test_dataloader,
+    optimizer,
+    scaler,
+    loss_fn,
+    epochs,
+    device,
 ):
     """Trains and tests a PyTorch model for a given number of epochs.
 
@@ -154,24 +162,29 @@ def train(
 
     Returns:
       A dictionary of training and testing loss as well as training and
-      testing accuracy metrics. Each metric has a value in a list for
+      testing performance metrics. Each metric has a value in a list for
       each epoch.
       In the form: {train_loss: [...],
-                    train_acc: [...],
+                    train_perform: [...],
                     test_loss: [...],
-                    test_acc: [...]}
+                    test_perform: [...]}
       For example if training for epochs=2:
                    {train_loss: [2.0616, 1.0537],
-                    train_acc: [0.3945, 0.3945],
+                    train_perform: [0.3945, 0.3945],
                     test_loss: [1.2641, 1.5706],
-                    test_acc: [0.3400, 0.2973]}
+                    test_perform: [0.3400, 0.2973]}
     """
     # Create empty results dictionary
-    results = {"train_loss": [], "train_acc": [], "test_loss": [], "test_acc": []}
+    results = {
+        "train_loss": [],
+        "train_perform": [],
+        "test_loss": [],
+        "test_perform": [],
+    }
 
     # Loop through training and testing steps for a number of epochs with tqdm progress bar
     for epoch in tqdm(range(epochs)):
-        train_loss, train_acc = train_step(
+        train_loss, train_perform = train_step(
             model=model,
             dataloader=train_dataloader,
             loss_fn=loss_fn,
@@ -179,7 +192,7 @@ def train(
             scaler=scaler,
             device=device,
         )
-        test_loss, test_acc = test_step(
+        test_loss, test_perform = test_step(
             model=model,
             dataloader=test_dataloader,
             loss_fn=loss_fn,
@@ -190,16 +203,16 @@ def train(
         print(
             f"Epoch: {epoch+1} | "
             f"train_loss: {train_loss:.4f} | "
-            f"train_acc: {train_acc:.4f} | "
+            f"train_perform: {train_perform:.4f} | "
             f"test_loss: {test_loss:.4f} | "
-            f"test_acc: {test_acc:.4f}"
+            f"test_perform: {test_perform:.4f}"
         )
 
         # Update results dictionary
         results["train_loss"].append(train_loss)
-        results["train_acc"].append(train_acc)
+        results["train_perform"].append(train_perform)
         results["test_loss"].append(test_loss)
-        results["test_acc"].append(test_acc)
+        results["test_perform"].append(test_perform)
 
     # Return the filled results at the end of the epochs
     return results
--- scripts/utils.py
+++ scripts/utils.py
@@ -6,30 +6,64 @@ import torch.nn as nn
 from pathlib import Path
 
 
-def save_model(model, target_dir, model_name):
-    """Saves a PyTorch model to a target directory.
+def save_checkpoint(state, target_dir, model_name):
+    """Saves a PyTorch model state(s) to a target directory.
 
     Args:
-      model (nn.Module): A PyTorch model to save.
+      state (nn.Module): State(s) of a PyTorch model to save.
       target_dir (str): A directory for saving the model to.
       model_name (str): A filename for the saved model. Should include
-        either ".pth" or ".pt" as the file extension.
+      ".pth", ".pt", ".pth.tar" or "pt.tar" as the file extension.
 
     Example usage:
-      save_model(model=model_0,
+      save_model(state=model,
                  target_dir="models",
-                 model_name="05_going_modular_tingvgg_model.pth")
+                 model_name="tipburn_resnet50_classifier.pth.tar")
     """
     # Create target directory
     target_dir_path = Path(target_dir)
     target_dir_path.mkdir(parents=True, exist_ok=True)
 
     # Create model save path
-    assert model_name.endswith(".pth") or model_name.endswith(
-        ".pt"
-    ), "model_name should end with '.pt' or '.pth'"
+    assert model_name.endswith(
+        (".pth", ".pt", ".pth.tar", "pt.tar")
+    ), "model_name should end with '.pth', '.pt', '.pth.tar' or 'pt.tar'"
     model_save_path = target_dir_path / model_name
 
     # Save the model state_dict()
-    print(f"[INFO] Saving model to: {model_save_path}")
-    torch.save(obj=model.state_dict(), f=model_save_path)
+    print(f"[INFO] Saving model states to: {model_save_path}")
+    torch.save(obj=state, f=model_save_path)
+
+
+def load_checkpoint(checkpoint, model):
+    """Loads a saved PyTorch model state.
+
+    Args:
+      checkpoint (str): File of saved model state including path if necessary.
+      model (nn.Module): PyTorch model on which the state will be applied.
+    """
+    print(f"[INFO] Opening model state: {checkpoint}")
+    model.load_state_dict(checkpoint["state_dict"])
+
+
+def class_accuracy(pred_logits, labels):
+    """Calculate classification accuracy.
+
+    Accuracy is determined as the fraction of predictions that match
+    the ground-truth labels.
+
+    Args:
+      pred_logits (torch.Tensor): Tensor of prediction logits.
+      labels (torch.Tensor): Tensor of ground truth labels.
+    """
+    # Check if predictions and labels are of the same length
+    assert len(pred_logits) == len(
+        labels
+    ), "Arguments pred_logits and labels should be tensors of the same length"
+
+    # Get predicted labels
+    pred_labels = pred_logits.argmax(dim=1)
+
+    # Calculate and return accuracy
+    pred_acc = (pred_labels == labels).sum().item() / len(pred_labels)
+    return pred_acc
