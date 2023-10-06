1d992d1d4c08914ed0e3840d8774f807de1e6c0e
Jacky To <145977609+JackyTomato@users.noreply.github.com>
2023-10-06 15:30:47 +0200

    Added data_preprocess for images

---
+++ scripts/data_preprocess.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Preprocesses the raw imaging data for use in deep learning.
+
+TODO:
+    - Add plant cropper
+    - Add background removal
+    - Figure out a way to deal with overlap
+"""
