80a70b3ad51b38a164ebe5d1174791dd24beee74
Jacky To <145977609+JackyTomato@users.noreply.github.com>
2023-10-11 16:59:11 +0200

    Added loop over dir and multi processing

--- scripts/data_preprocess.py
+++ scripts/data_preprocess.py
@@ -10,8 +10,29 @@ TODO:
 
 # Import statements
 from skimage import io, color, filters, segmentation, util, morphology
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+from multiprocessing.pool import Pool
+from tqdm import tqdm
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
+import imageio.plugins.pillow
+
+
+# Remove alpha channel - author Chris Dijkstra
+def no_alpha(rgb_im):
+    """Removes the alpha channel of an RGB image with 4 channels
+
+    Args:
+        rgb_im (numpy.ndarray): 4 dimensional array representing an RGB image
+
+    Returns:
+        numpy.ndarray: 3 dimensional array representing an RGB image
+    """
+    if rgb_im.shape[2] == 4:
+        alphaless = color.rgba2rgb(rgb_im)
+    return alphaless
 
 
 # Define backgrond removal functions - author: Chris Dijkstra
@@ -86,7 +107,7 @@ def water_hsv_thresh(rgb_im, n_seeds, h_th=0.0, s_th=0.0, v_th=0.0):
     """Segments an image based on hsv thresholds, after watershed averaging
 
     :param rgb_im: np.ndarray, 3 dimensional array representing an RGB image
-    :param n_seeds:
+    :param n_seeds: int, number of initialized seeds for watershedding
     :param h_th: float, the threshold for the hue channel, everything below this
         value is marked as background
     :param s_th: float, the threshold for the value channel, everything below
@@ -101,14 +122,100 @@ def water_hsv_thresh(rgb_im, n_seeds, h_th=0.0, s_th=0.0, v_th=0.0):
     return mask.astype(int)
 
 
-# Perform all operations
+# Define end-to-end background removal fucntion for multithread
+def path_back_mask(rgb_im_path, n_seeds, h_th=0.0, s_th=0.0, v_th=0.0):
+    """Masks background of image from the corresponding path
+
+    Uses hsv thresholds after watershed averaging for background segmentation.
+    This function compiles all necessary tasks from image path to mask in order
+    to be used in multiprocessing or multithreading,
+    as they require a single function from start to finish.
+
+    :param rgb_im_path: str, path of RGB image
+    :param n_seeds: int, number of initialized seeds for watershedding
+    :param h_th: float, the threshold for the hue channel, everything below this
+        value is marked as background
+    :param s_th: float, the threshold for the value channel, everything below
+        this value is marked as background
+    :param v_th: float, the threshold for the saturation channel everything
+        below this value is marked as background
+    :return: np.ndarray, 2D mask with the
+    """
+    # Read file, if can't read file, function returns nothing
+    try:
+        img = io.imread(rgb_im_path)
+    except:
+        print(f"[ISSUE] {rgb_im_path} was unreadable")
+        return
+    img = no_alpha(img)
+    back_mask = water_hsv_thresh(img, n_seeds, h_th, s_th, v_th)
+    back_mask = morphology.binary_opening(back_mask)
+    print(f"[INFO] Mask made for {rgb_im_path}!")
+    return back_mask
+
+
 def main():
-    image = io.imread("snapshots/tb_snap3.png")
+    # Create filepaths
+    data_dir = "/lustre/BIF/nobackup/to001/thesis_MBF/data/img/RGB"
+    image_names = os.listdir(data_dir)
+    filepaths = []
+    for image_name in image_names:
+        filepath = os.path.join(data_dir, image_name)
+        filepaths.append(filepath)
 
-    # Background segmentation
-    plant_mask = water_hsv_thresh(image, 1500, s_th=0.25, v_th=0.2)
-    plant_mask = morphology.binary_opening(plant_mask)
+    # for image_name in image_names:
+    #     filepath = os.path.join(data_dir, image_name)
+    #     image = io.imread(filepath)
+    #     image = no_alpha(image)
+    #     plant_mask = water_hsv_thresh(image, 1500, s_th=0.25, v_th=0.2)
+    #     plant_mask = morphology.binary_opening(plant_mask)
+    #     print(f"{image_name} succesfully masked!")
 
-    # Visualization
-    plt.imshow(plant_mask)
-    plt.show()
+    # Multi-processed background segmentation from filepaths
+    num_cores = 4
+    with Pool(processes=num_cores) as pool:
+        prepped_mask = partial(path_back_mask, n_seeds=1500, s_th=0.25, v_th=0.2)
+        process_iter = pool.imap(func=prepped_mask, iterable=filepaths)
+        process_loop = tqdm(process_iter, total=len(filepaths), position=0, leave=False)
+        count = 0
+        for mask in process_loop:
+            count += 1
+            print(f"Image number {count} succesfully masked!")
+
+    # Multi-threaded background segmentation from filepaths
+    # num_cores = 2
+    # with ThreadPoolExecutor(max_workers=num_cores) as pool:
+    #     prepped_mask = partial(path_back_mask, n_seeds=1500, s_th=0.25, v_th=0.2)
+    #     masks = list(
+    #         tqdm(
+    #             pool.map(prepped_mask, filepaths),
+    #             total=len(filepaths),
+    #             position=0,
+    #             leave=False,
+    #         )
+    #     )
+    #     count = 0
+    #     for mask in masks:
+    #         count += 1
+    #         print(f"Image number {count} succesfully masked!")
+
+    # filename = "51-1-Lettuce_Correct_Tray_09-RGB-FishEyeCorrected.png"
+    # filepath = os.path.join(data_dir, filename)
+    # image = io.imread(filepath)
+    # image = no_alpha(image)
+
+    # # Background segmentation
+    # plant_mask = water_hsv_thresh(image, 1500, s_th=0.25, v_th=0.2)
+    # plant_mask = morphology.binary_opening(plant_mask)
+
+    # # Visualization
+    # fig, axes = plt.subplots(1, 2)
+    # axes[0].imshow(image)
+    # axes[0].set_title("RGB")
+    # axes[1].imshow(plant_mask)
+    # axes[1].set_title("Background mask")
+    # plt.show()
+
+
+if __name__ == "__main__":
+    main()
