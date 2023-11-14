dfe2b022848085c6bd280899571c8b6fdb5bccf6
Jacky To <145977609+JackyTomato@users.noreply.github.com>
2023-11-14 12:33:06 +0100

    Defined binary2area, started main

---
+++ scripts/quantify_tb.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+Extract pixel areas from background and tipburn masks.
+Calculates tipburn parameters from pixel areas and outputs the results in a .tsv file.
+
+Assumes the mask files have been named in such a way that when reading the directories,
+and after using sorted() the list of filenames correspond perfectly between directories.
+Also assumes the mask names were saved as .png files.
+"""
+
+# Import statements
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import io
+
+
+# Define pixel area extractor
+def binary2area(img, area_value=1):
+    """Returns pixel area of pixels of interest from a binary image as NumPy array.
+
+    Args:
+        img (np.ndarray): Binary image, should be a NumPy array of ints, floats or bools.
+        area_value (int, optional): Value to retrieve area of, 0 or 1. Defaults to 1.
+
+    Returns:
+        int: Pixel area of pixels of interest, i.e. total count of the pixels of interest.
+    """
+    if 255.0 in img:
+        img[img == 255.0] = 1.0
+    area = np.count_nonzero(img == area_value)
+    return area
+
+
+def main():
+    # Define directories for loading and saving
+    bg_mask_dir = ""
+    tb_mask_dir = ""
+
+    target_dir = ""
+    values_save_name = ""
+
+    # Gather filenames, ignore non-.png files
+    bg_names = sorted(os.listdir(bg_mask_dir))
+    tb_names = sorted(os.listdir(tb_mask_dir))
+    bg_names = [name for name in bg_names if name.endswith(".png")]
+    tb_names = [name for name in tb_names if name.endswith(".png")]
+
+    # Loop through filenames, read images, extract pixel areas and calculate ratio
+    plant_areas = []
+    tb_areas = []
+
+    for bg_name, tb_name in zip(bg_names, tb_names):
+        # Create paths
+        bg_path = os.path.join(bg_mask_dir, bg_name)
+        tb_path = os.path.join(tb_mask_dir, tb_name)
+
+        # Read images
+        bg_img = io.imread(bg_path)
+        tb_img = io.imread(tb_path)
+
+        # Extract pixel areas
+        plant_area = binary2area(bg_img)
+        plant_areas.append(plant_area)
+        tb_area = binary2area(tb_img)
+        tb_areas.append(tb_area)
+
+
+if __name__ == "__main__":
+    main()
