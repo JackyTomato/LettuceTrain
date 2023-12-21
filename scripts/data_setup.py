0ce9c27325362f6355e7b7a5d5f96a6b58b5a858
Jacky To <145977609+JackyTomato@users.noreply.github.com>
2023-12-21 10:51:50 +0100

    Started pre TB dataset class

--- scripts/data_setup.py
+++ scripts/data_setup.py
@@ -4,12 +4,13 @@ Functionality for creating lettuce dataset as PyTorch Dataset
 and loading thre train & test datasets.
 
 TODO:
-    - Implement medium fusion
+    - Implement pre TB dataset
 """
 # Import statements
 import os
 import torch
 import numpy as np
+import re
 from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
 from torchvision import transforms, datasets
 from PIL import Image
@@ -20,7 +21,7 @@ from skimage.transform import resize
 import utils
 
 
-# Define Pytorch Dataset class for lettuce dataset
+# Define Pytorch Dataset class for lettuce segmentation dataset
 class LettuceSegDataset(Dataset):
     def __init__(
         self,
@@ -112,6 +113,239 @@ class LettuceSegDataset(Dataset):
                 elif len(match_ind) == 0:
                     mask_path = raw_name
                 mask_paths.append(mask_path)
+
+        # Also create lists of filepaths for Fm and FvFm if desired
+        if fm_dir is not None:
+            fm_paths = []
+            for fm_name in fm_names:
+                fm_path = os.path.join(fm_dir, fm_name)
+                fm_paths.append(fm_path)
+        if fvfm_dir is not None:
+            fvfm_paths = []
+            for fvfm_name in fvfm_names:
+                fvfm_path = os.path.join(fvfm_dir, fvfm_name)
+                fvfm_paths.append(fvfm_path)
+
+        # Split into train and test sets if desired when not doing K-fold cross validation
+        if (train_frac < 1) and (kfold is None):
+            if (fm_dir is None) and (fvfm_dir is None):
+                split = train_test_split(
+                    img_paths,
+                    mask_paths,
+                    train_size=train_frac,
+                    random_state=seed,
+                )
+
+                # Give train or test data as requested
+                if is_train:
+                    self.img_paths = split[0]
+                    self.mask_paths = split[2]
+                else:
+                    self.img_paths = split[1]
+                    self.mask_paths = split[3]
+            elif (fm_dir is not None) and (fvfm_dir is None):
+                split = train_test_split(
+                    img_paths,
+                    fm_paths,
+                    mask_paths,
+                    train_size=train_frac,
+                    random_state=seed,
+                )
+
+                # Give train or test data as requested
+                if is_train:
+                    self.img_paths = split[0]
+                    self.fm_paths = split[2]
+                    self.mask_paths = split[4]
+                else:
+                    self.img_paths = split[1]
+                    self.fm_paths = split[3]
+                    self.mask_paths = split[5]
+            elif (fm_dir is None) and (fvfm_dir is not None):
+                split = train_test_split(
+                    img_paths,
+                    fvfm_paths,
+                    mask_paths,
+                    train_size=train_frac,
+                    random_state=seed,
+                )
+
+                # Give train or test data as requested
+                if is_train:
+                    self.img_paths = split[0]
+                    self.fvfm_paths = split[2]
+                    self.mask_paths = split[4]
+                else:
+                    self.img_paths = split[1]
+                    self.fvfm_paths = split[3]
+                    self.mask_paths = split[5]
+            elif (fm_dir is not None) and (fvfm_dir is not None):
+                split = train_test_split(
+                    img_paths,
+                    fm_paths,
+                    fvfm_paths,
+                    mask_paths,
+                    train_size=train_frac,
+                    random_state=seed,
+                )
+
+                # Give train or test data as requested
+                if is_train:
+                    self.img_paths = split[0]
+                    self.fm_paths = split[2]
+                    self.fvfm_paths = split[4]
+                    self.mask_paths = split[6]
+                else:
+                    self.img_paths = split[1]
+                    self.fm_paths = split[3]
+                    self.fvfm_paths = split[5]
+                    self.mask_paths = split[7]
+
+        # Don't split when training fraction is 1 or when doing K-fold cross validation
+        else:
+            self.img_paths = img_paths
+            self.mask_paths = mask_paths
+            if fm_dir is not None:
+                self.fm_paths = fm_paths
+            if fvfm_dir is not None:
+                self.fvfm_paths = fvfm_paths
+
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, index):
+        # Check if Fm and FvFm should be included
+        fm_exists = hasattr(self, "fm_paths")
+        fvfm_exists = hasattr(self, "fvfm_paths")
+
+        # Retrieve image, should be np.array for albumentations.transforms
+        img = np.array(Image.open(self.img_paths[index]))
+
+        # Also retrieve Fm and FvFm images if desired
+        if fm_exists:
+            fm = np.array(Image.open(self.fm_paths[index]))
+            fm = fm / fm.max() * 255  # normalize Fm values as they are large
+            img_fit = resize(img, (fm.shape[0], fm.shape[1], 3), anti_aliasing=True)
+            fm = fm * (img_fit.sum(axis=2) > 0)  # apply RGB background mask
+        if fvfm_exists:
+            fvfm = np.array(Image.open(self.fvfm_paths[index]))
+            img_fit = resize(img, (fvfm.shape[0], fvfm.shape[1], 3), anti_aliasing=True)
+            fvfm = fvfm * (img_fit.sum(axis=2) > 0)  # apply RGB background mask
+
+        # Retrieve mask, mask could be .json or an image format
+        size = img.shape[:2]
+        if self.mask_paths[index].endswith(".json"):
+            mask = utils.binary_poly2px(self.mask_paths[index], custom_size=size)
+        elif self.mask_paths[index].endswith(
+            (".png", ".jpg", ".jpeg", ".tif", ".tiff")
+        ):
+            mask = np.array(Image.open(self.mask_paths[index]))
+        else:
+            mask = np.zeros(size, dtype=np.int32)  # Create empty mask for missing mask
+        if 255.0 in mask:
+            mask[mask == 255.0] = 1.0
+
+        # Apply data augmentation transforms to image, mask and optionally Fm and FvFm
+        if self.transform is not None:
+            if fm_exists or fvfm_exists:
+                # Apply augmentations
+                grayscales = [mask]
+                if fm_exists:
+                    grayscales.append(fm)
+                if fvfm_exists:
+                    grayscales.append(fvfm)
+                augmentations = self.transform(image=img, masks=grayscales)
+
+                # Retrieve augmentations
+                if (fm_exists) and (not fvfm_exists):
+                    mask, fm = augmentations["masks"]
+                elif (not fm_exists) and (fvfm_exists):
+                    mask, fvfm = augmentations["masks"]
+                elif (fm_exists) and (fvfm_exists):
+                    mask, fm, fvfm = augmentations["masks"]
+            else:
+                # Apply augmentations
+                augmentations = self.transform(image=img, mask=mask)
+
+                # Retrieve augmentations
+                mask = augmentations["mask"]
+            img = augmentations["image"]
+
+        # Compile resulting images, in a way suitable for fusion if desired
+        if fm_exists:
+            img = np.concatenate([img, fm[np.newaxis, :, :]], axis=0)
+        if fvfm_exists:
+            img = np.concatenate([img, fvfm[np.newaxis, :, :]], axis=0)
+        result = (img, mask)
+
+        # Also provide image name if desired
+        if self.give_name:
+            img_name = self.img_names[index].split(os.extsep)[0]
+            result.append(img_name)
+
+        return result
+
+
+# Create PyTorch dataset class for lettuce pretipburn classification
+class LettucePreTBClassDataset(Dataset):
+    def __init__(
+        self,
+        img_dir,
+        label_dir,
+        is_train,
+        fm_dir=None,
+        fvfm_dir=None,
+        train_frac=0.75,
+        kfold=None,
+        transform=None,
+        seed=42,
+        give_name=False,
+    ):
+        """Creates a PyTorch Dataset class of the lettuce pre-tipburn classification dataset.
+
+        Uses sorted() to match filenames of images and masks. Check if filenames
+        are constructed in such a way that sorted() will sort the filenames in the same way.
+        Otherwise images and masks will be mismatched when loading the data.
+
+        Fm and FvFm images can be included as additional input. The Fm and FvFm are stacked
+        with the RGB images as additional channels.
+
+        Allows for a training fraction of 1, in that case the whole dataset is provided.
+        When doing K-fold cross valiation, the whole dataset is also provided,
+        as the split will be performed downstream.
+
+        Args:
+            img_dir (str): Filepath of directory containing the images.
+            label_dir (str): Filepath of directory  containing the segmentation masks.
+            is_train (bool): If true, gives train data. If false, gives test data.
+            fm_dir (str, optional): Filepath of directory containing Fm images for fusion. Defaults to None.
+            fvfm_dir (str, optional): Filepath of directory containing FvFm images for fusion. Defaults to None.
+            train_frac (float, optional): Fraction of data that is train. Defaults to 0.75.
+            kfold (int, optional): K for K-fold cross validation. If not None, train_frac is ignored. Defaults to None.
+            transform (albumentations.Compose, optional): Transformations for data aug. Defaults to None.
+            seed (int, optional): Seed for reproducible train test split of data. Defaults to 42.
+            give_name (bool, optional): If True, dataset also provides image name.
+        """
+        self.transform = transform
+        self.give_name = give_name
+
+        # List all image and optionally Fm and FvFm filenames
+        self.img_names = sorted(os.listdir(img_dir))
+        if fm_dir is not None:
+            fm_names = sorted(os.listdir(fm_dir))
+        if fvfm_dir is not None:
+            fvfm_names = sorted(os.listdir(fvfm_dir))
+
+        # Create lists of filepaths for images and corresponding list of labels
+        img_paths = []
+        labels = []
+        for img_name in self.img_names:
+            # List image name paths
+            img_path = os.path.join(img_dir, img_name)
+            img_paths.append(img_path)
+            # List corresponding labels based on timepoint and trayID in filename
+
+        # Create corresponding list of pre-tipburn labels based on filename
 
         # Also create lists of filepaths for Fm and FvFm if desired
         if fm_dir is not None:
